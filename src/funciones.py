1f8fbdc6884d034de281bde2fa7f15a2a7758502
Jesús Figueroa Roldán <verdeblancoyazul@gmail.com>
2026-03-27 10:55:13 +0100

    Creación estructura básica del menú

---
+++ src/funciones.py
@@ -0,0 +1,38 @@
+import json
+
+# MENÚ
+
+def menu():
+    
+    menu = '''
+    1. Listado de equipos (Premier League 24-25)
+    2. Cantidad de partidos jugados por jornada
+    3. Listado de partidos de un equipo
+    4. Listado de los partidos ganados de un equipo
+    5. Equipo con más goles hasta la fecha
+    6. Salir
+    '''
+    
+    opcion = 0
+    
+    while opcion != 6:
+        print(menu)
+        opcion = int(input(">> Introduce una opción (1-6): "))
+
+# LISTAR INFORMACIÓN
+
+
+
+# CONTAR INFORMACIÓN
+
+
+
+# FILTRAR INFORMACIÓN
+
+
+
+# BUSCAR INFORMACIÓN RELACIONADA
+
+
+
+# EQUIPO CON MÁS GOLES
